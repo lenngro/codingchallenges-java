9eab14043fbf7955fd9e100f825f5245a24036ea
Lennart Grosser <lennart.grosser@web.de>
2019-03-27 08:23:49 +0100

    added Tree to Double Linked List

---
+++ TreeToDoubleLinkedList/src/com/lenngro/Node.java
@@ -0,0 +1,13 @@
+package com.lenngro;
+/* A binary tree node has data, and left and right pointers */
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
